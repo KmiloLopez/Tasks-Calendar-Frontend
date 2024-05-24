1e02056f95bae708c857cf16de653c0406572301
KmiloLopez <andres.lopez2018@outlook.es>
2024-05-24 11:00:18 -0500

    changed to localStorage

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -12,7 +12,7 @@ export const useAuth = () => {
 };
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);//accesso a usuario global
+  const [user, setUser] = useState(null); //accesso a usuario global
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [errors, setErrors] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -23,15 +23,15 @@ export const AuthProvider = ({ children }) => {
       const timer = setTimeout(() => {
         setErrors([]);
       }, 5000);
-      return () => clearTimeout(timer);//quita el timer en caso de cambio de pagina,para que no quede andando
+      return () => clearTimeout(timer); //quita el timer en caso de cambio de pagina,para que no quede andando
     }
   }, [errors]);
 
   const signup = async (user) => {
     try {
-      
       const res = await registerRequest(user);
       if (res.status === 200) {
+        localStorage.setItem("token", res.data.token);
         setUser(res.data);
         setIsAuthenticated(true);
       }
@@ -46,6 +46,8 @@ export const AuthProvider = ({ children }) => {
       const res = await loginRequest(user);
       setUser(res.data);
       setIsAuthenticated(true);
+      const token = res.data.token;
+      localStorage.setItem("token", token);
     } catch (error) {
       console.log(error);
       // setErrors(error.response.data.message);
@@ -53,22 +55,30 @@ export const AuthProvider = ({ children }) => {
   };
 
   const logout = () => {
-    Cookies.remove("token");
+    //Cookies.remove("token");
+    localStorage.removeItem("token");
     setUser(null);
     setIsAuthenticated(false);
   };
 
   useEffect(() => {
-    const checkLogin = async () => {//se pone dentro de una funcion para poder poner el asyng await, esto para lo asincrono 
-      const cookies = Cookies.get();
-      if (!cookies.token) {//si no hay cookie.token...
+    const checkLogin = async () => {
+      //se pone dentro de una funcion para poder poner el asyng await, esto para lo asincrono
+      // const cookies = Cookies.get();
+      // if (!cookies.token) {
+      //   //si no hay cookie.token...
+      //   setIsAuthenticated(false);
+      //   setLoading(false);
+      //   return;
+      // }
+      const token = localStorage.getItem("token");
+      if (!token) {
         setIsAuthenticated(false);
         setLoading(false);
         return;
       }
-
       try {
-        const res = await verifyTokenRequest(cookies.token);//si hay un token este se envia al backend para ser verificado
+        const res = await verifyTokenRequest(token); //si hay un token este se envia al backend para ser verificado
         console.log(res);
         if (!res.data) return setIsAuthenticated(false);
         setIsAuthenticated(true);
@@ -81,7 +91,7 @@ export const AuthProvider = ({ children }) => {
     };
     checkLogin();
   }, []);
-//forma de exportacion
+  //forma de exportacion
   return (
     <AuthContext.Provider
       value={{
